45a08d583ccc21789547f3368971b63a72444ffd
manan-jadhav <manan-jadhav@users.noreply.github.com>
2023-04-26 13:11:19 +0000

    jekyll build from Action 629290246ae3332a2ef33c084928c8b7ec24c05b

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Hello World!",
+        "excerpt":"An obligatory Hello World post. Not much to see here.    https://xkcd.com/2656/  ","categories": [],
+        "tags": [],
+        "url": "/2023/04/22/hello-world.html",
+        "teaser": null
+      },{
+        "title": "Kubernetes with kOps and Terraform",
+        "excerpt":"Recently, at CodeGem we migrated our infrastructure from AWS ECS (Elastic Container Service) to our own Kubernetes cluster. Why we made that change is a post of its own, but a quick TL;DR would be: We needed scheduling capabilites - like K8s CronJobs - but ECS did not natively provide...","categories": ["Kubernetes","DevOps"],
+        "tags": ["DevOps","K8s","Kubernetes","Terraform","AWS","ECS","Automation"],
+        "url": "/kubernetes/devops/2023/04/24/kubernetes-with-kops-and-terraform.html",
+        "teaser": null
+      }]
